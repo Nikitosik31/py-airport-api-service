fb21f77ee4b48815a949b671e9c81628589c4941
Nik <nikita.pitushkin31@mail.com>
2026-07-01 11:15:42 +0300

    feat: create apps airport and user and implements airport models

---
+++ airport/models.py
@@ -0,0 +1,122 @@
+import os
+import uuid
+
+from django.db import models
+
+from django.conf import settings
+from django.utils.text import slugify
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='cities')
+
+    def __str__(self):
+        return self.name
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='airports')
+
+    def __str__(self):
+        return f"Airport: {self.name}. Closest city: {self.city.name}."
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+def airport_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/airplanes/", filename)
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE, related_name='airplanes')
+    image = models.ImageField(null=True, upload_to=airport_image_file_path)
+
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_row
+
+class Route(models.Model):
+    source = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='source_routes')
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='destination_routes')
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source} -> {self.destination}"
+
+class Flight(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name='flights')
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE, related_name='flights')
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crew = models.ManyToManyField(Crew, blank=True, related_name='flights')
+    flight_number = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.route} ({self.departure_time})"
+
+    @property
+    def duration(self):
+        return (self.arrival_time - self.departure_time)
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orders')
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return str(self.created_at)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name='tickets')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='tickets')
+
+    class Meta:
+        unique_together = (('row', 'seat', 'flight',),)
+        ordering = ['-order']
+
+
+    def __str__(self):
+        return f"{self.flight} - row {self.row}, seat {self.seat}"
+
